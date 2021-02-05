2ee94add7c35af0b3208add533f4447397546a67
Joshua Peavler <joshuaapeavler@gmail.com>
2021-02-05 16:01:40 -0700

    Setup DAL and API layers for creating a new bug report.

--- data/bugReports.js
+++ data/bugReports.js
@@ -44,8 +44,8 @@ const getBugReportById = (id) => {
                 const collection = db.collection(colName);
                 try {
                     const _id = new ObjectID(id);
-                    const result = await collection.findOne({_id});
-                    if(result) {
+                    const result = await collection.findOne({_id}); //look for the bug report with the requested Mongo ObjectID
+                    if(result) {    //If the idea is found
                         resolve(result);
                     } else {
                         reject({ error: "ID not found in database" });
@@ -60,7 +60,34 @@ const getBugReportById = (id) => {
     return myPromise;
 }
 
+//Create a new bug report function
+const addBugReport = (bugReport) => {
+    const myPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if(err) {
+                reject(err);
+            } else {
+                console.log("Connected to db for CREATE");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.insertOne(bugReport, (err, result) => {
+                    if(err) {
+                        reject(err);
+                    } else {
+                        let response = {insertedCount : result.insertedCount};
+                        response.insertedItem = result.ops[0];
+                        resolve(response);
+                        client.close();
+                    }
+                });
+            }
+        });
+    });
+    return myPromise;
+}
+
 module.exports = {
     getBugReports,
-    getBugReportById
+    getBugReportById,
+    addBugReport
 }
--- routes/api/bugReports.js
+++ routes/api/bugReports.js
@@ -1,31 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const {getBugReports, getBugReportById} = require('../../data/bugReports');
+const {getBugReports, getBugReportById, addBugReport} = require('../../data/bugReports');
 
 //Get Routers
-    router.get("/", async (req, res) => {   //Get all active bug reports
-        try {
-            const bugReports = await getBugReports();   //accessing data from database takes time
-            res.send(bugReports);
-        } catch (err) { //Only error will be serverside since no bad request can be made to this route
-            console.log(err); //Print out detailed error message
-            res.status(500).send("Internal server issue, check logs");  //Send server error code and prompt to check details. Hopefully never seen by end user.
-        }
-    });
+router.get("/", async (req, res) => {   //Get all active bug reports
+    try {
+        const bugReports = await getBugReports();   //accessing data from database takes time
+        res.send(bugReports);
+    } catch (err) { //Only error will be serverside since no bad request can be made to this route
+        console.log(err); //Print out detailed error message
+        res.status(500).send("Internal server issue, check logs");  //Send server error code and prompt to check details. Hopefully never seen by end user.
+    }
+});
 
-    router.get("/:id", async (req, res) => {
-        try {
-            const bugReport = await getBugReportById(req.params.id);
-            res.send(bugReport);
-        } catch (err) { //Error can be caused by server or by client.
-            if(err.error) { //If the dal sent an object with an error message detailing a client's bad request
-                res.status(400).send(err);
-            } else {    //The error is caused by poorly written code on the server. Hopefully never happens with end user.
-                console.log(err);
-                res.status(500).send("Internal server issues, check logs");
-            }
+router.get("/:id", async (req, res) => {
+    try {
+        const bugReport = await getBugReportById(req.params.id);
+        res.send(bugReport);
+    } catch (err) { //Error can be caused by server or by client.
+        if(err.error) { //If the dal sent an object with an error message detailing a client's bad request
+            res.status(400).send(err);
+        } else {    //The error is caused by poorly written code on the server. Hopefully never happens with end user.
+            console.log(err);
+            res.status(500).send("Internal server issues, check logs");
         }
-    });
-//
+    }
+});
+//Post router
+router.post("/", async (req, res) => {
+    try {
+        const newReport = await addBugReport(req.body);
+        res.send(newReport);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal server issue, check logs");
+    }
+});
+
 
 module.exports = router;
