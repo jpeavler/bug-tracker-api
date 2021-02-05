b845d6fc7e759eeb2b75dce634fba478ceb90a5e
Joshua Peavler <joshuaapeavler@gmail.com>
2021-02-05 14:43:50 -0700

    Setup routers for users and bug reports and integrated cors.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/api/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/api/users');
+const bugReportsRouter = require('./routes/api/bugReports');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,8 +21,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
+//Express generated view routers
 app.use('/', indexRouter);
+//My written API routers
+app.use('/api/users', usersRouter);
+app.use('/api/bugReports', bugReportsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+//Get Routers
+
+//
+
+module.exports = router;
