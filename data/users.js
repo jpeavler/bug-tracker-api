f1eea00c9caac63f044c66e3f4653efc029fa33b
Joshua Peavler <joshuaapeavler@gmail.com>
2021-02-11 12:26:32 -0700

    Created DAL function to add a new user to database.

--- data/users.js
+++ data/users.js
@@ -68,7 +68,35 @@ const getUserByMongoId = (id) => {
     return myPromise;
 }
 
+//Create a new user function
+//param bugReport is an object describing the new user you wish to add to the database
+const addUser = (user) => {
+    const myPromise = new Promise((resolve, reject) => {    //resolve handles the desired data, reject handles when something goes wrong.
+        MongoClient.connect(url, settings, function(err, client) {
+            if(err) {   //if database connection fails
+                reject(err);
+            } else {
+                console.log("Connected to db for CREATE");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.insertOne(user, (err, result) => {  //Attempt to insert the new user
+                    if(err) {   //if insertOne MongoDB function fails, reject the promise with an error message.
+                        reject(err);
+                    } else {
+                        let response = {insertedCount : result.insertedCount}; //Wrap the newly added user in an object with description
+                        response.insertedItem = result.ops[0];
+                        resolve(response);
+                        client.close(); //Make sure to end connection to database since we are done accessing it
+                    }
+                });
+            }
+        });
+    });
+    return myPromise;
+}
+
 module.exports = {
     getUsers,
-    getUserByMongoId
+    getUserByMongoId,
+    addUser
 }
